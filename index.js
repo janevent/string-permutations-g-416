26feb79d7b95e9d85f1f354fcf8684b280444dba
Jane Venter <janeanneventer@gmail.com>
2020-09-02 05:24:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function findAllPermutations(theString){
   for(let i = 0; i < theString.length; i++){
     let char = theString[i];
     let remainingChars = theString.slice(0, i) + theString.slice(i+1);
+    for(let permutation of findAllPermutations(remainingChars)){
+      arrayContainer.push(char + permutation)
+    }
   }
   return arrayContainer
 }
