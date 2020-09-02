8ffc0cf55e71da5f444abb6ad25b16792fc6815f
Jane Venter <janeanneventer@gmail.com>
2020-09-02 05:05:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function findAllPermutations(theString){
+  if(theString.length < 2){
+    return [theString];
+  }
   let array = theString.split("");
   let arrayContainer = [];
   array.forEach((letter)=>{
